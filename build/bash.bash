b55809ace418db2b0c2ed1d2abd18557787fd87f
Warren Blanchet <me@wskb.ca>
2025-07-23 03:13:17 +0000

    Refactored into functions

--- build/bash.bash
+++ build/bash.bash
@@ -2,12 +2,24 @@
 
 SCRIPT_DIR="$(cd $(dirname "$0"); pwd)"
 
-DEST="${DEST:-$SCRIPT_DIR/../dist6}"
+DEST="${DEST:-$SCRIPT_DIR/../dist}"
+
+copy_folder_to_file () {
+    local file="$1"
+    local slug="$2"
+    local src="$3"
+    local target="$4"
+
+    echo "echo \"Updating ${file}\"" >> "$target"
+    echo "cat > \"${file}\" <<'$slug'" >> "$target"
+    for A in `find "$src" -type f | sort`; do
+        cat "$A" >> "$target"
+    done
+    echo "$slug" >> "$target"
+}
 
 mkdir -p "$DEST"
 
 cp "$SCRIPT_DIR/../content/install/1-prefix.bash" "$DEST/bash-setup"
-for A in `find "$SCRIPT_DIR/../content/bash" -type f | sort`; do
-    cat "$A" >> "$DEST/bash-setup"
-done
-echo "END_BASHRC_WSKB" >> "$DEST/bash-setup"
+
+copy_folder_to_file '$HOME/.bashrc.wskb' 'END_BASHRC_WSKB' "$SCRIPT_DIR/../content/bash" "$DEST/bash-setup"
