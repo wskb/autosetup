d4a85b64edd264265a5847063b1109d4c714bb4d
Warren Blanchet <me@wskb.ca>
2025-07-24 00:08:47 +0000

    Add promptColor selection

--- content/install/1-prefix.bash
+++ content/install/1-prefix.bash
@@ -21,6 +21,51 @@ check_writeable () {
     fi
 }
 
+showPromptColors () {
+	echo "index - fgbg"
+ 
+    local offset=$(( $RANDOM % 49 ))
+    local colorIndex
+    local loopIndex
+	for (( loopIndex = 0; loopIndex < 49; loopIndex++ )); do
+        local colorIndex=$(( ($loopIndex + $offset) % 49 ))
+        local fgColor=$((colorIndex % 7))
+        local bgColor=$((colorIndex / 7))
+        if [[ $fgColor -ge $bgColor ]]; then
+            fgColor=$((fgColor + 1))
+        fi
+        if [[ $colorIndex -lt 10 ]]; then
+            echo -n " $colorIndex "
+        else
+            echo -n "$colorIndex "
+        fi
+        echo -e "\\033[3${fgColor};4${bgColor}m\\c"
+        echo -n " fc ${fgColor} - bc ${bgColor} |"
+        echo -e "\\033[1m\\c"
+        echo -n " fc ${fgColor} - bc ${bgColor} "
+        echo -e "\\033[0m\\c"
+        echo
+    done
+}
+
+choosePromptColors () {
+    local target="$1"
+
+    showPromptColors
+
+    local selectedIndex=-1
+    until [[ selectedIndex -ge 0 && selectedIndex -le 49 ]]; do
+        read -p "Pick by number: " selectedIndex
+    done
+    
+    local fgColor=$((selectedIndex % 7))
+    local bgColor=$((selectedIndex / 7))
+    if [[ $fgColor -ge $bgColor ]]; then
+        fgColor=$((fgColor + 1))
+    fi
+    echo -n $fgColor$bgColor > "$target"
+}
+
 check_writeable "$HOME/.bashrc.wskb"
 check_writeable "$HOME/.inputrc"
 
@@ -35,6 +80,13 @@ if [[ ( ! -e "$HOME/.bash_profile" ) || ( ! grep -q  ".bashrc" "$HOME/.bash_prof
     edit_bash_profile=1
     edit_bashrc
     check_writeable "$HOME/.bash_profile"
+fi
+
+if [[ -e "$HOME/.promptColor" ]]; then
+    echo "skipping prompt color selection as file exists: $HOME/.promptColor"
+else
+    check_writeable "$HOME/.promptColor"
+    choosePromptColors "$HOME/.promptColor"
 fi
 
 if [[ $edit_bash_profile -eq 1 ]]; then
