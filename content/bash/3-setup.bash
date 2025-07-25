a944501f74e1707cdc9f972746c5e325212619bb
Warren Blanchet <me@wskb.ca>
2025-07-25 02:53:33 +0000

    Add missing alias

--- content/bash/3-setup.bash
+++ content/bash/3-setup.bash
@@ -1,1 +1,2 @@
 alias wskb-refresh-bash="bash --init-file <( curl -sfL https://raw.githubusercontent.com/wskb/autosetup/refs/heads/dist/bash-setup )"
+alias wskb-vscode-diff="curl -sfL https://raw.githubusercontent.com/wskb/autosetup/refs/heads/dist/vscode-diff | bash -"
